38be3e9182483a8d50cbc9a4087c0f93bda5f12d
AurelianStefan <frontendjavascript@C3-28.local>
2023-01-19 20:45:37 +0200

    curs9

---
+++ curs8/JS/script.js
@@ -0,0 +1,54 @@
+// pas 1 adauga elemente
+var nrProduse = 0;
+function addProduct() {
+  let product = document.getElementById("product").value;
+  if (!product) return;
+  const text =
+    "<li class='product'>" +
+    product +
+    "<span id='" +
+    nrProduse +
+    "'>Sterge</span></li>";
+  document.getElementById("list").insertAdjacentHTML("beforeend", text);
+  document.getElementById("product").value = "";
+  nrProduse = nrProduse + 1;
+  if (typeof (Storage) !== "undefined") {
+    // store
+    localStorage.setItem(nrProduse, text);
+    localStorage.setItem("numarProduse", nrProduse);
+  }
+}
+
+const button = document.getElementById("add");
+button.addEventListener("click",  addProduct);
+
+// PAS 2 selectam /deselectam produsele
+document.getElementById('list').addEventListener('click', function(event){
+    const element = event.target;
+    element.classList.toggle("done");
+});
+
+// pas 3 afisam produsele din local storage 
+window.onload=function(){
+    var nr= (localStorage.getItem('numarProduse'));
+    let i;
+    if (typeof(Storage)!=="undefined" && nr>0){
+        nrProduse=Number(nr);
+        for (i=1; i<=nr; i++){
+            // Retrieve
+            document.getElementById("list").insertAdjacentHTML('beforeend', localStorage.getItem (i));
+        }
+
+    }
+    else {
+        nrProduse=0;
+    }
+}
+document.getElementById('list').addEventListener('dbclick', function (event){
+    const element=event.target;
+    let storageKey=Number(element.id)+1;
+    nrProduse=nrProduse-1;
+    localStorage.removeItem(storageKey)
+    localStorage.setItem('nrProduse', nrProduse);
+        location.reload()
+})
---
+++ curs9/unitTest/JS/math.js
@@ -0,0 +1,19 @@
+function hello(){
+    return 'hello world';
+}
+function add(a,b){
+    let suma=a+b;
+    return suma;
+}
+// pentru numere intregi
+function add(a,b){
+    a= a*100;
+    b=b*100;
+
+    let suma=(a+b)/100;
+    return suma;
+}
+// pentru numere reale, cu cifre dupa punct 
+function inm (a,b){
+    return a*b;
+}
